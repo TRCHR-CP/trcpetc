55d983c2fae5703604eb97ad57530c4d6fa02553
Weiss, Jessica <Jessica.Weiss@uhn.ca>
2025-10-03 16:51:52 -0400

    Created package data and data dict

---
+++ R/data.r
@@ -0,0 +1,60 @@
+
+#' Cardiology Patient Dataset
+#'
+#' A synthetic dataset of 185 patients who underwent cardiac surgery.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item PatientID: Unique identifier for each patient
+#'   \item Age: Age in years
+#'   \item Sex: Biological sex
+#'   \item NYHA: NYHA functional class
+#'   \item EjectionFraction: Ejection fraction (%)
+#'   \item BMI: Body mass index (kg/m²)
+#'   \item HeartRate: Heart rate (bpm)
+#'   \item SBP: Systolic blood pressure (mmHg)
+#'   \item DBP: Diastolic blood pressure (mmHg)
+#'   \item SmokingStatus: Smoking status: Never, Former, Current
+#'   \item HistoryMI: History of myocardial infarction
+#'   \item SurgeryDate: Date of cardiac surgery
+#'   \item TransplantDate: Date of heart transplant
+#'   \item DeathDate: Date of death
+#'   \item LastVisitDate: Date of last clinical visit
+#'   \item Diabetes: Presence of diabetes
+#'   \item Hypertension: Presence of hypertension
+#'   \item COPD: Presence of COPD
+#'   \item CKD: Presence of chronic kidney disease
+#'   \item CAD: Presence of coronary artery disease
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cardio_data
+#' @usage data(cardio_data)
+#' @format A data frame with 185 rows and 20 variables
+NULL
+
+
+
+
+#' Cardiology Patient Dataset dictionary
+#'
+#' A synthetic dataset of 185 patients who underwent cardiac surgery.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item VariableName: Name of the variable in the dataset
+#'   \item Label: Manuscript-ready label
+#'   \item Description: Detailed description of the variable
+
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cardio_data_dictionary
+#' @usage data(cardio_data_dictionary)
+#' @format A data frame with 20 rows and 3 columns:
+NULL
+
+
+
