981ae50a04c0cf336a128f13d0c6e5bdd7e17edd
Weiss, Jessica <Jessica.Weiss@uhn.ca>
2025-10-06 16:50:19 -0400

    Saving functions in seperate r scripts

---
+++ R/table_one.R
@@ -0,0 +1,221 @@
+#' @title table_one
+#' @description Generates a table of summary statistics for descriptive analysis.
+#' @details
+#' The `table_one` function computes summary statistics for continuous, logical, and factor variables,
+#' following the statistical reporting guidelines of the *Annals of Medicine*. If a grouping variable is provided,
+#' the function can also evaluates between-group differences. The input data frame should consist only of numeric,
+#' logical, and factor variables. Factor variables with only two levels should be converted to logical variables.
+#' Date and datetime variables should be excluded.
+
+#'
+#' @param df A data frame consisting of numeric, logical, and factor variables with or without a grouping variable
+#' @param group Name of the grouping variable (optional).
+#' @param datadic datadic A data frame serving as a data dictionary, containing variable names and their descriptions.
+#' @param var_name the column name of `data_dict` that
+#'   contains the variable names. Only required if the column name is not "var_name"
+#' @param var_desp the column name of `data_dict` that
+#'   contains the variable descriptions Only required if the column name is not "var_desp"
+#' @param seed An optional seed value for reproducibility of p-values
+#' @param include_overall Character string specifying whether and how to include an overall summary.
+#'   Must be one of:
+#'   \itemize{
+#'     \item `"none"`: Do not include an overall summary (default).
+#'     \item `"group"`: Include an overall summary only for observations with non-missing values in the grouping variable.
+#'     \item `"all"`: Include an overall summary for all observations, regardless of missingness in the grouping variable.
+#'   }
+#'   Default is `"none"`.
+#' @param total Logical; whether to report the total N. Default is `TRUE`.
+#' @param  pval Logical; whether to report p-values for between-group comparisons. Default is `TRUE`.
+#' @param print_test Logical. If TRUE, the output will include the type of statistical test applied to each variable. Default is `FALSE`.
+#' @param  continuous Character string specifying the summary statistics for continuous variables.
+#'  Must be one of:
+#'   \itemize{
+#'     \item `"mediqr"`: Median and interquartile range.
+#'     \item `"meansd"`: Mean and standard deviation.
+#'     \item `"c("mediqr","meansd")"`: Both median/IQR and mean/SD.
+#'   }
+#' @param round_to_100 force rounded total to add up to 100 calculated using the largest remainder method for factor variables
+#' @param drop.unused.levels Removes factor variables with 0. Levels with zero are not included in statistical tests
+#' @param  kable_output Logical; if `TRUE`, outputs a formatted `kable` table including variable descriptions, N, statistics, and p-values.
+#' @return A data frame containing summary statistics by variable type, optionally stratified by group and formatted for reporting, or a formatted kable table if `kable_output = TRUE`..
+#' @examples
+#' table_one(df = cardio_data, group = Sex)
+#' @export
+#' @importFrom rlang enquo quo_is_missing
+#' @importFrom dplyr select
+#'
+table_one <- function(df, group, datadic = NULL, var_name, var_desp, seed = 123, include_overall  = c("none","group","all"),
+                      total = TRUE,pval=TRUE,print_test  = FALSE,continuous = "mediqr",round_to_100 = FALSE,
+                      drop.unused.levels = FALSE,
+                      kable_output =TRUE,caption = NULL,overall_label = "Overall",include_Missing = FALSE,
+                      Check_box = NULL,Check_box_title = NULL) {
+
+  set.seed(seed)
+
+
+
+
+  # Setting up  -------------------------------------------------------------------------
+
+
+  include_overall <- match.arg(include_overall)
+  op <- options(warn = -1)
+  on.exit(options(op))
+
+  group <- rlang::enquo(group)
+  var_name <- rlang::enquo(var_name)
+  var_desp <- rlang::enquo(var_desp)
+
+  if (rlang::quo_is_missing(var_name)) var_name <- rlang::quo(var_name)
+  if (rlang::quo_is_missing(var_desp)) var_desp <- rlang::quo(var_desp)
+
+  if(rlang::quo_is_missing(group)) pval <- FALSE #No p-values without a grouping variable
+  if(!pval) print_test <- FALSE #Can't print the test is there is no pvalue
+
+  # Errors -------------------------------------------------------------------------
+
+  ## Only allows mediqr & meansd for continuous variables
+
+  invalid_continuous <- continuous[!continuous %in% c("mediqr", "meansd")]
+  if (length(invalid_continuous) > 0) {
+    stop(paste0('Invalid value(s) in "continuous": ', paste(unique(invalid_continuous), collapse = ", "),
+                '. Allowed values are "mediqr" and "meansd".'))
+  }
+
+  #Overall name can not match a group name
+  if((!rlang::quo_is_missing(group) & include_overall %in% c("group","all")) & overall_label %in% (df %>% dplyr::pull(!!group) %>% unique() %>% stats::na.omit())) {
+    stop(paste0("`overall_label` ('", overall_label, "') cannot match an existing level in the grouping variable `"))
+  }
+
+
+  #Grouped summary table -------------------------------------------------------------------------
+
+  if(!rlang::quo_is_missing(group)){
+    summary_group <- table_one_stratify(df,group = !!group,total = total,round_to_100 = round_to_100,drop.unused.levels = drop.unused.levels)
+
+
+    # Including a missing column and p-value -------------------------------------------------------------------------
+
+
+    if(include_Missing){
+      summary_group <-   df %>% dplyr::mutate(
+        {{ group }} := {{ group }} %>%
+          forcats::fct_na_value_to_level(level = "Missing")) %>%
+        table_one_stratify(group = !!group,total = total,round_to_100 = round_to_100,drop.unused.levels = drop.unused.levels) %>%
+        dplyr::left_join((summary_group %>% select(row_id,pval)),by = c('row_id'),suffix = c(".Missing",".No.Missing"))
+
+    }
+
+  }
+
+
+  # Overall summary table -------------------------------------------------------------------------
+
+  ##If include overall = "group" remove rows where the grouping variable is missing
+  if(!rlang::quo_is_missing(group) & include_overall == "group"){
+    df <- df %>% dplyr::filter(!is.na(!!group))
+  }
+
+
+  ##Remove the grouping variable from the overall table
+  if(!rlang::quo_is_missing(group)){
+    df <- df %>% select(-!!group)
+  }
+
+  if(rlang::quo_is_missing(group) |include_overall == "all" | include_overall == "group" ){
+    summary_overall <- table_one_overall(df,total = total,round_to_100 = round_to_100,overall_label = overall_label,drop.unused.levels = drop.unused.levels)
+  }
+
+
+  # Combining summary tables together -------------------------------------------------------------------------
+
+  if(!rlang::quo_is_missing(group) & (include_overall == "all" | include_overall == "group" )){
+    summary <- summary_overall %>% dplyr::left_join(summary_group,by = c('row_id','variable','type'))
+  }else if(rlang::quo_is_missing(group)){
+    summary <- summary_overall
+  }else {
+    summary <- summary_group
+  }
+
+  ##Run the grouped table (If include_overall is include_overall = all or include_overall = group)
+
+
+  # Removing pre-defined columns (pval, continuous) -------------------------
+
+
+
+  if(!pval) summary$pval = summary$pval.Missing = summary$pval.No.Missing <- NULL
+  if(!print_test ) summary$print_test  <- NULL
+
+  #Optionally removing the continuous variables
+  if(!"meansd" %in% (continuous)) summary <- summary %>% dplyr::filter(!grepl("_meansd$", row_id))
+  if(!"mediqr" %in% (continuous)) summary <- summary %>% dplyr::filter(!grepl("_mediqr$", row_id))
+
+
+  # Adding the datadic and cleaning the table -------------------------------------------------------------------------
+
+
+  if (is.null(datadic)) {
+    out <- summary %>%
+      dplyr::select(row_id, variable, type,
+                    dplyr::ends_with("n"), dplyr::ends_with("stat"), dplyr::everything()) %>%
+      # dplyr::select(-!!var_desp) %>%
+      dplyr::mutate(type = ifelse(is.na(type) & row_id==variable,
+                                  gsub("(^[[:lower:]])", "\\U\\1", variable, perl=TRUE), type),
+                    type = ifelse(type %in% c("meansd", "mediqr"),
+                                  gsub("(^[[:lower:]])", "\\U\\1", variable, perl=TRUE), type),
+                    type = ifelse(row_id==paste0(variable, "TRUE"),
+                                  gsub("(^[[:lower:]])", "\\U\\1", variable, perl=TRUE), type)) %>%
+      dplyr::rename(`var_desp`= type)
+  } else {
+    out <- summary %>%
+      dplyr::left_join(dplyr::select(datadic, !!var_name, !!var_desp),
+                       by = c("variable"= rlang::quo_name(var_name))) %>%
+      dplyr::mutate(type = ifelse(is.na(type) & row_id==variable, !!var_desp, type), # factor
+                    type = ifelse(type %in% c("meansd", "mediqr"), !!var_desp, type), # continuous
+                    type = ifelse(row_id==paste0(variable, "TRUE"), !!var_desp, type), # logical
+      ) %>%
+      dplyr::select(row_id, variable, type,
+                    dplyr::ends_with("n"), dplyr::ends_with("stat"), dplyr::everything()) %>%
+      dplyr::select(-!!var_desp) %>%
+      dplyr::rename(`var_desp`= type)
+  }
+
+  ##Formatting check box rows to indent
+  out <- out   %>%
+    dplyr::mutate(dplyr::across(
+      dplyr::ends_with("_n"),
+      ~ dplyr::if_else(variable %in% Check_box, NA, .)
+    ))
+
+  ##Formatting title rows to
+  out <- out   %>%
+    dplyr::mutate(dplyr::across(
+      dplyr::ends_with("_stat"),
+      ~ dplyr::if_else(variable %in% Check_box_title, NA, .)
+    ))
+
+
+
+  # out <- out %>%
+  #   dplyr::mutate(dplyr::across(
+  #     c(test, pval),
+  #     ~ dplyr::if_else(variable %in% Check_box_title, NA, .)
+  #   ))
+
+
+
+  # Creating a kable table -------------------------------------------------------------------------
+
+
+
+  if(kable_output){
+
+    out <-  kable_table_one(out,pval = pval,include_Missing = include_Missing,print_test = print_test,total=total,caption=caption)
+
+  }
+
+  out
+
+}
+
---
+++ R/table_one_set_up.R
@@ -0,0 +1,31 @@
+titles_non_missing <- function(df, columns, new_col_name = "Title") {
+  df %>%
+    dplyr::mutate(
+      {{ new_col_name }} := dplyr::if_else(
+        rowSums(!is.na(dplyr::across(dplyr::all_of(columns)))) > 0,
+        TRUE,
+        NA
+      )
+    ) %>%
+    dplyr::relocate({{ new_col_name }}, .before = dplyr::all_of(columns[1]))
+}
+
+#' @title check_box_convert
+#' @description To add checkbox questions to a table 1 in R
+#' @export
+
+check_box_convert <- function(df, check_box_cols, title = NULL) {
+  out <- df %>%
+    dplyr::mutate(dplyr::across(dplyr::all_of(check_box_cols), ~ as.logical(.x))) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(dplyr::across(dplyr::all_of(check_box_cols), ~ {
+      if (all(c_dplyr::across(dplyr::all_of(check_box_cols)) == FALSE)) NA else .x
+    })) %>%
+    dplyr::ungroup()
+
+  if (!is.null(title)) {
+    out <- titles_non_missing(out, columns = check_box_cols, new_col_name = !!title)
+  }
+
+  return(out)
+}
